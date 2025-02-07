1cb6235fc4dd66772fc659ff44a8b4d7587c39ce
Chenyuan Yang <cy1yang@outlook.com>
2025-02-07 15:40:47 -0600

    add the step to reuse our generated spec

--- spec-gen/eval_spec.py
+++ spec-gen/eval_spec.py
@@ -383,6 +383,7 @@ if __name__ == "__main__":
         "--kernel-config",
         type=str,
         help="the kernel config used to make shared linux build",
+        default="",
     )
     args = parser.parse_args()
 
